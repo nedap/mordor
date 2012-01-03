8bb1bee6782db192bbe6b9deb1ae7216d6925b74
jwkoelewijn <jwkoelewijn@gmail.com>
2012-01-03 16:34:28 +0100

    added check for empty timestamp

--- lib/mordor/resource.rb
+++ lib/mordor/resource.rb
@@ -71,7 +71,7 @@ module Mordor
             timestamp_value = BSON::Timestamp.new(timestamp_value["seconds"], timestamp_value["increment"])
           end
           ordered_self_hash = BSON::OrderedHash.new
-          ordered_self_hash[timestamp_attribute] = (timestamp_value.nil?) ? BSON::Timestamp.new(0, 0) : timestamp_value
+          ordered_self_hash[timestamp_attribute] = (timestamp_value.nil? || timestamp_value.empty?) ? BSON::Timestamp.new(0, 0) : timestamp_value
           self_hash.each do |key, value|
             ordered_self_hash[key] = value
           end
